185f6e207c1fe96912b9d749da8e6df6e4f98ab5
Sebastian Unger <basti.unger@googlemail.com>
2023-11-14 13:28:06 +0100

    Added example Using PCA

--- example.py
+++ example.py
@@ -13,7 +13,7 @@ quit()
 ### imports of standard libs
 from os.path import join
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt #### NEED version 2.2.3
 import os
 
 ### import of the tda package
@@ -39,16 +39,84 @@ c = tda.fit(spc_train)
 plt.figure()
 plt.imshow(np.reshape(tda.lens.values, shape_im))
 #%%
+tda.draw_network()
+tda.draw_cluster_network()
 
-import numpy as np
-from scipy.cluster.hierarchy import linkage, fcluster, dendrogram, maxdists
-from scipy.spatial.distance import pdist
+#%%
+import networkx as nx
+import itertools
+from skimage import exposure
 
-idd = pdist(my_data, tda.cluster_metric)
-idd[np.isnan(idd)] = 0
-Z = linkage(idd, tda.cluster_method)
+cl = set(tda.partition.values())
+part = np.asarray(list(tda.partition.values()))
+ids = nx.get_node_attributes(tda.topograph, 'ids')
+id_topo = np.asarray(list(ids.keys()))
 
-fig = plt.figure(figsize=(25, 10))
+clmap = []
+for ic in cl:
+    nodes_c = id_topo[part==ic]
+    my_nodes = []
+    for node in nodes_c: my_nodes.append(list(ids[node]))
+    my_nodes = list(set(itertools.chain(*my_nodes)))
+    if len(my_nodes)>1:
+        
+        as_map = np.zeros(np.prod(shape_im))
+        as_map[my_nodes] = 1
+        
+        as_map = as_map * np.sum(spc_train, axis = -1)
+        as_map = (as_map-np.amin(as_map))/(np.amax(as_map)-np.amin(as_map))
+        as_map = exposure.equalize_hist(as_map)
+        im = np.reshape(as_map, shape_im)
+        clmap.append(im)
+        
+        if np.sum(my_nodes)>2:
+            plt.figure()
+            plt.imshow(im)
 
-dn = dendrogram(Z)
-md = maxdists(Z)
+#%%
+tda.draw_network()
+tda.draw_cluster_network()
+
+import networkx as nx
+import itertools
+from skimage import exposure
+
+cl = set(tda.partition.values())
+part = np.asarray(list(tda.partition.values()))
+ids = nx.get_node_attributes(tda.topograph, 'ids')
+id_topo = np.asarray(list(ids.keys()))
+
+clmap = []
+for ic in cl:
+    nodes_c = id_topo[part==ic]
+    my_nodes = []
+    for node in nodes_c: my_nodes.append(list(ids[node]))
+    my_nodes = list(set(itertools.chain(*my_nodes)))
+    if len(my_nodes)>0:
+        
+        as_map = np.zeros(np.prod(shape_im))
+        as_map[my_nodes] = 1
+        
+        clmap.append(as_map)
+
+reddim = np.array(clmap).T
+from sklearn.decomposition import PCA
+pca_fun = PCA(n_components=20)
+pca_fun.fit(reddim)
+values = pca_fun.transform(reddim).T
+
+evr = pca_fun.explained_variance_ratio_
+values = values[evr > 0.01]
+
+i =0
+for v in values:
+    print(i)
+    i=i+1
+    #as_map = v * np.sum(spc_train, axis = -1)
+    as_map=v
+    #as_map = (as_map-np.amin(as_map))/(np.amax(as_map)-np.amin(as_map))
+    #as_map = exposure.equalize_hist(as_map)
+    im = np.reshape(as_map, shape_im)
+        
+    plt.figure()
+    plt.imshow(im)
